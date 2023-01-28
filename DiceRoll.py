0cc119315a7feafa1954ddd0b9cf11ee6c726ab0
Aswin Harish <56496128+AswinHarish@users.noreply.github.com>
2023-01-28 22:39:56 +0530

    Add files via upload

---
+++ DiceRoll.py
@@ -0,0 +1,39 @@
+import random
+a = random.randint(1,6)
+	
+if a == 1:
+		print("[-----]")
+		print("[     ]")
+		print("[  ●  ]")
+		print("[     ]")
+		print("[-----]")
+if a == 2:
+		print("[-----]")
+		print("[  ●  ]")
+		print("[     ]")
+		print("[  ●  ]")
+		print("[-----]")
+if a == 3:
+		print("[-----]")
+		print("[     ]")
+		print("[● ● ●]")
+		print("[     ]")
+		print("[-----]")
+if a == 4:
+		print("[-----]")
+		print("[ ● ● ]")
+		print("[     ]")
+		print("[ ● ● ]")
+		print("[-----]")
+if a == 5:
+		print("[-----]")
+		print("[ ● ● ]")
+		print("[  ●  ]")
+		print("[ ● ● ]")
+		print("[-----]")
+if a == 6:
+		print("[-----]")
+		print("[● ● ●]")
+		print("[     ]")
+		print("[● ● ●]")
+		print("[-----]")
